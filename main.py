58a72e565d77971aa993ad11d4b3a2f7d1f95a52
Ashray <97219299+Ash-1111@users.noreply.github.com>
2022-01-06 14:43:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import discord
+from discord.ext import commands, tasks
+from discord import Embed
+from itertools import cycle
+import json
+import keep_alive
+from sys import exit
+import sqlite3
+from dotenv import load_dotenv
+import asyncio
+
+
+client.run('ODQ5MzMxOTY3MTk1MzQ5MDQz.YLZoCA.zkHQXQDEXwzEUp4YxdkFmEclMZE')
